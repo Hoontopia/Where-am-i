d6063f55c83bf4a42776e5d90a036d926806de62
Limsunghun <limsung1541@naver.com>
2016-07-11 15:14:10 +0900

    Update

---
+++ app/src/main/java/com/example/sec/whereami/API_Key.java
@@ -0,0 +1,8 @@
+package com.example.sec.whereami;
+
+/**
+ * Created by Factory on 2016-07-11.
+ */
+public class API_Key {
+    static String key = "4b84abed6f29d6e833dc233ec40243ed";
+}
--- app/src/main/java/com/example/sec/whereami/MainActivity.java
+++ app/src/main/java/com/example/sec/whereami/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sec.whereami;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -9,10 +10,16 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapReverseGeoCoder;
 import net.daum.mf.map.api.MapView;
 
-public class MainActivity extends AppCompatActivity implements MapView.CurrentLocationEventListener {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements MapView.CurrentLocationEventListener, TextToSpeech.OnInitListener, MapReverseGeoCoder.ReverseGeoCodingResultListener {
     MapView mapView;
+    MapReverseGeoCoder mReverseGeoCoder;
+    TextToSpeech _tts;
+    boolean _ttsActive = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
         setContentView(R.layout.activity_main);
 
         mapView = new MapView(this);
-        mapView.setDaumMapApiKey("4b84abed6f29d6e833dc233ec40243ed");
+        mapView.setDaumMapApiKey(API_Key.key);
         mapView.setCurrentLocationEventListener(this);
 
         RelativeLayout container = (RelativeLayout) findViewById(R.id.map_view);
@@ -37,18 +44,21 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading); // 좌표 추적모드 On
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading); // 좌표 추적모드 On
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onCurrentLocationUpdate(MapView mapView, MapPoint mapPoint, float v) {
-        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
-
-        Toast.makeText(MainActivity.this, "위도 : "+mapPointGeo.latitude+"\n경도 : "+mapPointGeo.longitude, Toast.LENGTH_SHORT).show();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        mReverseGeoCoder = new MapReverseGeoCoder(API_Key.key, mapView.getMapCenterPoint(), this, this);
+        mReverseGeoCoder.startFindingAddress();
         // 현재위치 바뀔때마다 Toast
     }
-
     @Override
     public void onCurrentLocationDeviceHeadingUpdate(MapView mapView, float v) {
 
@@ -62,5 +72,56 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
     @Override
     public void onCurrentLocationUpdateCancelled(MapView mapView) {
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        _tts = new TextToSpeech(getApplicationContext(), this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try{
+            if(_tts != null){
+                _tts.stop();
+                _ttsActive = false;
+            }
+        }catch (Exception e){
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            if (_tts != null) {
+                _tts.shutdown();
+                _tts = null;
+            }
+        }catch(Exception e){
+        }
+    }
+    @Override
+    public void onInit(int status) {
+    }
+
+    @Override
+    public void onReverseGeoCoderFoundAddress(MapReverseGeoCoder mapReverseGeoCoder, String s) {
+        mapReverseGeoCoder.toString();
+        onFinishReverseGeoCoding(s);
+    }
+
+    @Override
+    public void onReverseGeoCoderFailedToFindAddress(MapReverseGeoCoder mapReverseGeoCoder) {
+        onFinishReverseGeoCoding("Fail");
+    }
+
+    private void onFinishReverseGeoCoding(String result) {
+        Toast.makeText(this, "Reverse Geo-coding : " + result, Toast.LENGTH_SHORT).show();
+        _tts.setLanguage(Locale.KOREA);
+        _ttsActive=true;
+        _tts.speak("이름 : "+ result, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
