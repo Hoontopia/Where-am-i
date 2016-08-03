81e9a89ef925ad8aa9dea845e0548845e353728e
LimSungHun <limsung1541@naver.com>
2016-08-03 17:36:04 +0900

    init

--- app/src/main/java/com/example/sec/whereami/API_Key.java
+++
@@ -1,9 +0,0 @@
-package com.example.sec.whereami;
-
-/**
- * Created by Factory on 2016-07-11.
- */
-public class API_Key {
-    static final String DAUM_KEY = "4b84abed6f29d6e833dc233ec40243ed";
-    static final String GOOGLE_KEY = "AIzaSyDXbKJ9KHJQT-NHJGAVueQPAFuPaa_g73s";
-}
